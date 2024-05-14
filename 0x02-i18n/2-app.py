e3696537b9821f6c8569748c99dd4d0ac1472364
Amine Lamuadni <W7equwE5@gmail.com>
2024-05-14 22:47:39 +0100

    Add 2-app.py file

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+This module configures a Flask application with Flask-Babel to determine the
+preferred locale from the client request.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration class for Flask app."""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Select a language translation to use based on the client request."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Render a basic HTML template."""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
