42a8f15c9b38222abe74792e8c3b2432c93165b6
Amine Lamuadni <W7equwE5@gmail.com>
2024-05-14 22:57:44 +0100

    Update 2-app.py file

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -10,9 +10,9 @@ from flask_babel import Babel
 
 class Config:
     """Configuration class for Flask app."""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
@@ -20,17 +20,16 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    """Render a basic HTML template."""
-    return render_template('2-index.html')
-
-
-@babel.localeselector
 def get_locale() -> str:
     """Select a language translation to use based on the client request."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """Render a basic HTML template."""
+    return render_template('2-index.html')
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000', debug=True)
