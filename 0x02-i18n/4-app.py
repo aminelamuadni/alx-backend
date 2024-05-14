46fc9b6d4b5fb4d5683822733edaff07886d0b65
Amine Lamuadni <W7equwE5@gmail.com>
2024-05-15 00:21:29 +0100

    Add 4-app.py file

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+This module configures a Flask application with Flask-Babel for
+internationalization, including a URL parameter to force a particular locale.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """Configuration class for Flask app."""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Select a language translation to use based on the client request or URL
+    parameter.
+    """
+    user_locale = request.args.get('locale')
+    if user_locale and user_locale in app.config['LANGUAGES']:
+        return user_locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Render a localized HTML template."""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
