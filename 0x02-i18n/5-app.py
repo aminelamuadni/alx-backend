fd777f87a2dea4f801d868c843215df0dda379c0
Amine Lamuadni <W7equwE5@gmail.com>
2024-05-15 13:29:26 +0100

    Update 5-app.py file

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -58,7 +58,7 @@ def get_locale() -> str:
         str: The selected language translation to use.
     """
     user_locale = request.args.get('locale')
-    if user_locale in app.config['LANGUAGES']:
+    if user_locale and user_locale in app.config['LANGUAGES']:
         return user_locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
